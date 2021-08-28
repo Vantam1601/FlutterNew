abcbc65ec799b3061aadc0d2dc734699695b970b
nguyentri97@gmail.com <nguyentri97@gmail.com>
2021-08-28 11:33:29 +0700

    test flutter in vscode

--- lib/car.dart
+++ lib/car.dart
@@ -1,11 +1,9 @@
 abstract class Car {
-  /**
-   * Thuộc tính
-   * - Khai báo giá trị khỏi tạo nếu không sẽ báo lỗi
-   * - Nếu không khai báo giá trị khởi tạo thì phải thêm ? đằng sau kiểu dữ liệu
-   * - ? sau kiểu dữ liệu: có thế có giá trị null
-   * - ! sau value: chắc chắc value đó không có null
-   */
+  /// Thuộc tính
+  /// - Khai báo giá trị khỏi tạo nếu không sẽ báo lỗi
+  /// - Nếu không khai báo giá trị khởi tạo thì phải thêm ? đằng sau kiểu dữ liệu
+  /// - ? sau kiểu dữ liệu: có thế có giá trị null
+  /// - ! sau value: chắc chắc value đó không có null
 
   String? name;
   int? year;
@@ -13,6 +11,7 @@ abstract class Car {
   Car({
     this.name,
     this.year
+    
 });
 
   showInfo();
--- lib/main.dart
+++ lib/main.dart
@@ -1,81 +1,61 @@
 import 'package:flutter/material.dart';
-import 'calculation.dart';
-import 'car.dart';
-import 'mec.dart';
 
 void main() {
-//  numbers.forEach((number) {
-//    print(number);
-//  });
-
-//  stringNumbers.forEach((str) {
-//    print(str);
-//  });
-  var myCar = Merc(
-    name:'Mercedes G63',
-    year: 2020,
-    color: 'Blue'
+  runApp(
+    const MaterialApp(
+      home: Page1(),
+      debugShowCheckedModeBanner: false,
+    ),
   );
-  myCar.showInfo();
-  List<String> number1 = ['a','b'];
-  List<String> number2 = ['c','d'];
-  List<String> number3 = ['e','f'];
-//  print(myCar.totalNumber(number1,number2,number3));
+}
 
+class Page1 extends StatelessWidget {
+  const Page1({Key? key}) : super(key: key);
 
-  List<Merc> mers = <Merc>[
-    Merc(name:'G63',year: 2020,color: 'black'),
-    Merc(name:'AMG',year: 2020,color: 'black'),
-    Merc(name:'C200',year: 2021,color: 'black'),
-    Merc(name:'C500',year: 2020,color: 'yewllow'),
-  ];
-
-  mers.sort((a,b)=>a.name!.compareTo(b.name!));
-  //mers.sort((a,b)=>a.year!-b.year!);
-
-
-  //Update item
-  //var filterCar = mers.where((car)=>car.year == 2020 && car.name!.contains("Mercedes")).toList();
-
-  var b;
-  b ??= 5;
-
-  int? a;
-  a ??= 5;
-
-//  var totalNumber = (var a, var b) {
-//    return a + b;
-//  };
-//  print(totalNumber(1,2));
-
-  var iterable = new Iterable.generate(3);
-    print('su dung iterable render so');
-//  for (var item in iterable) {
-//    print(item);
-//  }
-  iterable.forEach((element) {
-    print(element);
-  });
-
-  print('==>>> Su dung iterable ');
-  for (var item in mers) {
-    print(item);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.of(context).push(_createRoute());
+          },
+          child: const Text('Go!'),
+        ),
+      ),
+    );
   }
+}
 
-  var itemToMap = {
-    'name': 'tri',
-    'age' : '24'
-  };
-  print('==>> Map');
-  for(var key in itemToMap.keys) {
-    print('$key :  ${itemToMap[key]}');
-  }
+Route _createRoute() {
+  return PageRouteBuilder(
+    pageBuilder: (context, animation, secondaryAnimation) => const Page2(),
+    transitionsBuilder: (context, animation, secondaryAnimation, child) {
+      const begin = Offset(0.0, 1.0);
+      const end = Offset.zero;
+      const curve = Curves.ease;
 
-  runApp (
-    Center(child: Text(
-      'Texxt',
-      style: TextStyle(fontSize: 20),
-      textDirection: TextDirection.ltr,
-    )),
+      var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+
+      return SlideTransition(
+        position: animation.drive(tween),
+        child: child,
+      );
+    },
   );
+}
+
+class Page2 extends StatelessWidget {
+  const Page2({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: const Center(
+        child: Text('Page 2'),
+      ),
+    );
+  }
 }
