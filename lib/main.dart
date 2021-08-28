e1c0142969bfd0226363ecc92ac9ea5ba0d3071a
nguyentri97@gmail.com <nguyentri97@gmail.com>
2021-08-28 17:07:02 +0700

    push code

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'calculation.dart';
 import 'car.dart';
 
-
 void main() {
 //  numbers.forEach((number) {
 //    print(number);
@@ -20,23 +19,24 @@ void main() {
 //  var totalNumber = totalNumbers(7, 9);
 
   List<Car> cars = <Car>[
-    Car(name:'Mercedes - G63',year: 2020),
-    Car(name:'Mercedes - AMG',year: 2020),
-    Car(name:'Mercedes - C200',year: 2021),
-    Car(name:'Mercedes - C500',year: 2020),
-    Car(name:'Audi',year: 2021),
-    Car(name:'Lambogrini',year: 2019)
+    Car(name: 'Mercedes - G63', year: 2020),
+    Car(name: 'Mercedes - AMG', year: 2020),
+    Car(name: 'Mercedes - C200', year: 2021),
+    Car(name: 'Mercedes - C500', year: 2020),
+    Car(name: 'Audi', year: 2021),
+    Car(name: 'Lambogrini', year: 2019)
   ];
 
   //cars.sort((a,b)=>a.name!.compareTo(b.name!));
- //cars.sort((a,b)=>a.year!-b.year!);
+  //cars.sort((a,b)=>a.year!-b.year!);
   //Update item
 
-  cars.add(Car(name:'Royal',year: 2020));
+  cars.add(Car(name: 'Royal', year: 2020));
   //var filterCar = cars.where((car)=>car.year == 2020 && car.name!.contains("Mercedes")).toList();
 
-  runApp (
-    Center(child: Text(
+  runApp(
+    Center(
+        child: Text(
       cars.toString(),
       style: TextStyle(fontSize: 20),
       textDirection: TextDirection.ltr,
