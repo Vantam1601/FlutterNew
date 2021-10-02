990304c4060cdb876cab13f12b28b3126db0eef9
nguyentri97@gmail.com <nguyentri97@gmail.com>
2021-10-02 11:41:35 +0700

    add image url

---
+++ lib/utils/image_url.dart
@@ -0,0 +1,118 @@
+const String bg_banner = "assets/images/bg_banner.png";
+const String bg_banner_2x = "assets/images/bg_banner@2x.png";
+const String bg_banner_3x = "assets/images/bg_banner@3x.png";
+
+const String chat = "assets/images/chat.png";
+const String chat_2x = "assets/images/chat@2x.png";
+const String chat_3x = "assets/images/chat@3x.png";
+
+const String coupon_dark = "assets/images/coupon-dark.png";
+const String coupon_dark_2x = "assets/images/coupon-dark@2x.png";
+const String coupon_dark_3x = "assets/images/coupon-dark@3x.png";
+
+const String coupon = "assets/images/coupon.png";
+const String coupon_2x = "assets/images/coupon@2x.png";
+const String coupon_3x = "assets/images/coupon@3x.png";
+
+const String imgCateDefault = "assets/images/imgCateDefault.png";
+const String imgCateDefault_2x = "assets/images/imgCateDefault@2x.png";
+const String imgCateDefault_3x = "assets/images/imgCateDefault@3x.png";
+
+const String logo = "assets/images/logo.png";
+const String logo_2x = "assets/images/logo@2x.png";
+const String logo_3x = "assets/images/logo@3x.png";
+
+const String logo_dark = "assets/images/logo-dark.png";
+const String logo_dark_2x = "assets/images/logo-dark@2x.png";
+const String logo_dark_3x = "assets/images/logo-dark@3x.png";
+
+const String map = "assets/images/map.png";
+const String map_2x = "assets/images/map@2x.png";
+const String map_3x = "assets/images/map@3x.png";
+
+const String map_and_flags = "assets/images/map_and_flags.png";
+const String map_and_flags_2x = "assets/images/map_and_flags@2x.png";
+const String map_and_flags_3x = "assets/images/map_and_flags@3x.png";
+
+const String mask = "assets/images/mask.png";
+const String mask_2x = "assets/images/mask@2x.png";
+const String mask_3x = "assets/images/mask@3x.png";
+
+const String kDefalut = "assets/images/kDefalut.png";
+const String kDefalut_2x = "assets/images/kDefalut@2x.png";
+const String kDefalut_3x = "assets/images/kDefalut@3x.png";
+
+const String search_dark = "assets/images/searchDark.png";
+const String search_dark_2x = "assets/images/searchDark@2x.png";
+const String search_dark_3x = "assets/images/searchDark@3x.png";
+
+const String search_light = "assets/images/searchLight.png";
+const String search_light_2x = "assets/images/searchLight@2x.png";
+const String search_light_3x = "assets/images/searchLight@3x.png";
+
+const String unconnected = "assets/images/unconnected.png";
+const String unconnected_2x = "assets/images/unconnected@2x.png";
+const String unconnected_3x = "assets/images/unconnected@3x.png";
+
+const String ux = "assets/images/ux.png";
+const String ux_2x = "assets/images/ux@2x.png";
+const String ux_3x = "assets/images/ux@3x.png";
+
+const String wave = "assets/images/wave.png";
+const String wave_2x = "assets/images/wave@2x.png";
+const String wave_3x = "assets/images/wave@3x.png";
+
+const String wave_dark = "assets/images/wave-dark.png";
+const String wave_dark_2x = "assets/images/wave-dark@2x.png";
+const String wave_dark_3x = "assets/images/wave-dark@3x.png";
+
+const String gateway_bacs = "assets/images/gateway/bacs.png";
+const String gateway_bacs_2x = "assets/images/gateway/bacs@2x.png";
+const String gateway_bacs_3x = "assets/images/gateway/bacs@3x.png";
+
+const String gateway_cheque = "assets/images/gateway/cheque.png";
+const String gateway_cheque_2x = "assets/images/gateway/cheque@2x.png";
+const String gateway_cheque_3x = "assets/images/gateway/cheque@3x.png";
+
+const String gateway_cod = "assets/images/gateway/cod.png";
+const String gateway_cod_2x = "assets/images/gateway/cod@2x.png";
+const String gateway_cod_3x = "assets/images/gateway/cod@3x.png";
+
+const String gateway_paypal = "assets/images/gateway/paypal.png";
+const String gateway_paypal_2x = "assets/images/gateway/paypal@2x.png";
+const String gateway_paypal_3x = "assets/images/gateway/paypal@3x.png";
+
+const String gateway_razorpay = "assets/images/gateway/razorpay.png";
+const String gateway_razorpay_2x = "assets/images/gateway/razorpay@2x.png";
+const String gateway_razorpay_3x = "assets/images/gateway/razorpay@3x.png";
+
+const String gateway_stripe = "assets/images/gateway/stripe.png";
+const String gateway_stripe_2x = "assets/images/gateway/stripe@2x.png";
+const String gateway_stripe_3x = "assets/images/gateway/stripe@3x.png";
+
+const String gateway_stripe_suport = "assets/images/gateway/stripesuport.png";
+const String gateway_stripe_suport_2x =
+    "assets/images/gateway/stripesuport@2x.png";
+const String gateway_stripe_suport_3x =
+    "assets/images/gateway/stripesuport@3x.png";
+
+const String getting_start_get_start_1 =
+    "assets/images/getting-start/get-start-1.png";
+const String getting_start_get_start_1_2x =
+    "assets/images/getting-start/get-start-1@2x.png";
+const String getting_start_get_start_1_3x =
+    "assets/images/getting-start/get-start-1@3x.png";
+
+const String getting_start_get_start_2 =
+    "assets/images/getting-start/get-start-2.png";
+const String getting_start_get_start_2_2x =
+    "assets/images/getting-start/get-start-2@2x.png";
+const String getting_start_get_start_2_3x =
+    "assets/images/getting-start/get-start-2@3x.png";
+
+const String getting_start_get_start_3 =
+    "assets/images/getting-start/get-start-3.png";
+const String getting_start_get_start_3_2x =
+    "assets/images/getting-start/get-start-3@3x.png";
+const String getting_start_get_start_3_3x =
+    "assets/images/getting-start/get-start-3@3x.png";
