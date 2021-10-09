a15f7c68443fc1a0196725523f5f86b212f0d54a
tien-it <69241666+tien-it@users.noreply.github.com>
2021-10-09 09:56:21 +0700

    1
    1

---
+++ android/app/src/main/kotlin/com/example/demoplashscreen/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.demoplashscreen
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,45 +1,25 @@
+
+
+import 'package:demoplashscreen/intro_slider.dart';
 import 'package:flutter/material.dart';
-import 'calculation.dart';
-import 'car.dart';
-
+//import 'splashscreen.dart';
+//ham main
 void main() {
-//  numbers.forEach((number) {
-//    print(number);
-//  });
-
-//  stringNumbers.forEach((str) {
-//    print(str);
-//  });
-//  var myCar = Car(
-//    name:'Mercedes G63',
-//    year: 2020,
-//  );
-//  myCar.doSomething();
-//  myCar.sayHello(personName: 'Tri');
-//  var totalNumber = totalNumbers(7, 9);
-
-  List<Car> cars = <Car>[
-    Car(name: 'Mercedes - G63', year: 2020),
-    Car(name: 'Mercedes - AMG', year: 2020),
-    Car(name: 'Mercedes - C200', year: 2021),
-    Car(name: 'Mercedes - C500', year: 2020),
-    Car(name: 'Audi', year: 2021),
-    Car(name: 'Lambogrini', year: 2019)
-  ];
-
-  //cars.sort((a,b)=>a.name!.compareTo(b.name!));
-  //cars.sort((a,b)=>a.year!-b.year!);
-  //Update item
-
-  cars.add(Car(name: 'Royal', year: 2020));
-  //var filterCar = cars.where((car)=>car.year == 2020 && car.name!.contains("Mercedes")).toList();
-
-  runApp(
-    Center(
-        child: Text(
-      cars.toString(),
-      style: TextStyle(fontSize: 20),
-      textDirection: TextDirection.ltr,
-    )),
-  );
+  runApp(const MyApp());
 }
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      //goi splash screen
+      home: IntroSliderPage(),
+    );
+  }
+}
+//class splash screen
+
+
+
